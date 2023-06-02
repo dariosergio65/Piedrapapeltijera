d0d3e5a6916e6e3ce18e738d7d1df2b8ec4f6947
Dario Moreda <dariosergio@hotmail.com>
2023-06-02 16:26:30 -0300

    prueba1

--- js/juego.js
+++ js/juego.js
@@ -1,8 +1,19 @@
 
 //onclick="ini()" debe estar en el button inicio
-
+/*
 function ini(){ //funcion que se ejecuta al hacer click en inicio
 
   $('#pantalla1').hide();
   $('#pantalla2').show();
 }
+*/
+  // el button debe tener el id "inicio"
+
+  $(document).ready(function() {
+    $("#inicio").click(function() {
+
+      $('#pantalla1').hide();
+      $('#pantalla2').show();
+      
+    });
+  });
